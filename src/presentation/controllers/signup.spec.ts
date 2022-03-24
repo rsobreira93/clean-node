71f50d6dd05aea9b0d0b1bbc165904ec8f8a0d84
rsobreira93 <sobreiraromuloes@gmail.com>
2022-03-24 15:40:41 -0300

    test: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,4 +1,5 @@
 import { InvalidParamError } from '../errors/invalid-param-error';
+import { ServerError } from '../errors/servere-error';
 import { EmailValidator } from '../protocols/email-validator';
 import { MissingParamError } from './../errors/missing-param-error';
 import { SignUpController } from './signup';
@@ -112,5 +113,28 @@ describe('SingUp Controller', () => {
     };
     sut.handle(httpRequest);
     expect(isValid).toHaveBeenCalledWith('any_email@mail.com');
+  });
+
+  test('Should return 500 if EmailValidator throws', () => {
+    class EmailValidatorStub implements EmailValidator {
+      isValid(email: string): boolean {
+        throw new Error();
+      }
+    }
+
+    const emailValidatorStub = new EmailValidatorStub();
+    const sut = new SignUpController(emailValidatorStub);
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new ServerError());
   });
 });
