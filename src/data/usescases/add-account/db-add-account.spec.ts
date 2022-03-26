1ea84b3be96153f1573d0a123cee7db1ff0f57a7
rsobreira93 <sobreiraromuloes@gmail.com>
2022-03-26 08:47:30 -0300

    refactor: move make sut to a helper interface

--- src/data/usescases/add-account/db-add-account.spec.ts
+++ src/data/usescases/add-account/db-add-account.spec.ts
@@ -1,14 +1,29 @@
+import { Encrypter } from '../../protocols/encypter';
 import { DbAddAccount } from './db-add-account';
+
+interface SutTypes {
+  sut: DbAddAccount;
+  encryptStub: Encrypter;
+}
+
+const makeSut = (): SutTypes => {
+  class EncrypterStub {
+    async encrypt(password: string): Promise<string> {
+      return new Promise((resolve) => resolve('hashed_password'));
+    }
+  }
+  const encryptStub = new EncrypterStub();
+  const sut = new DbAddAccount(encryptStub);
+
+  return {
+    sut,
+    encryptStub,
+  };
+};
 
 describe('DbAccount Usecase', () => {
   test('Should call Encrypter with correct password', async () => {
-    class EncrypterStub {
-      async encrypt(password: string): Promise<string> {
-        return new Promise((resolve) => resolve('hashed_password'));
-      }
-    }
-    const encryptStub = new EncrypterStub();
-    const sut = new DbAddAccount(encryptStub);
+    const { encryptStub, sut } = makeSut();
     const encryptSpy = jest.spyOn(encryptStub, 'encrypt');
     const accountData = {
       name: 'valid_name',
